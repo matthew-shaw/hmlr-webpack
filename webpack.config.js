17afa8bd221d0388cad48ff0e185e0b4ecedf51f
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-03-29 15:02:49 +0000

    skip link and service nav

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   devServer: {
     static: path.resolve(__dirname, "dist"),
     compress: true,
-    port: 9000
+    port: 9000,
   },
   entry: ["./src/scss/main.scss"],
   output: {
