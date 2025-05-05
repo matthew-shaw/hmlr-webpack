4c206e5061a54be7d79344960ede5f40b4e29de9
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-05-05 17:04:10 +0100

    govuk-frontend v5.10 release

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const { PurgeCSSPlugin } = require("purgecss-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const glob = require("glob");
@@ -16,7 +17,6 @@ module.exports = {
   devtool: "source-map",
   devServer: {
     static: path.resolve(__dirname, "dist"),
-    compress: true,
     port: 9000,
   },
   entry: ["./src/js/main.mjs", "./src/scss/main.scss"],
@@ -78,6 +78,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new CompressionPlugin({ test: /(\.js|\.css)(\?.*)?$/i }),
     new MiniCssExtractPlugin({
       filename: "hmlr.[contenthash].min.css",
     }),
